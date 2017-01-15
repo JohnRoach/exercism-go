6e7bd7816ffa6d0d01288fc38789206c10f40e3e
JohnRoach <johnroach1985@gmail.com>
2017-01-15 12:51:07 -0500

    Adding gigasecond solution bench marked at 0.33 ns/op

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,10 +1,12 @@
-// Package clause.
+// Package gigasecond was added here for reasons...
 package gigasecond
 
+import "time"
+
 // Constant declaration.
-const testVersion = ? // find the value in gigasecond_test.go
+const testVersion = 4
 
-// API function.  It uses a type from the Go standard library.
-func AddGigasecond(time.Time) time.Time
-
-// Reviewers don't think much of stub comments.  Replace or remove.
+// AddGigasecond tell you when your time is up
+func AddGigasecond(birthTime time.Time) time.Time {
+	return time.Unix(birthTime.Unix()+1000000000, 0)
+}
