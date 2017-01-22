feed672ad1dcdbcea8f9ed1fed2f8ac5820ea726
JohnRoach <johnroach1985@gmail.com>
2017-01-21 22:51:10 -0500

    Adding hamming distance example

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,7 +1,26 @@
 package hamming
 
+import "fmt"
+
 const testVersion = 5
 
+// Distance returns the hamming distance between two strings
 func Distance(a, b string) (int, error) {
-	// your code here!
+	aLength := len(a)
+	bLength := len(b)
+
+	if aLength != bLength {
+		return -1, fmt.Errorf("Strings %s and %s are not of equal length", a, b)
+	}
+
+	distance := 0
+
+	for index := range a {
+		if a[index] != b[index] {
+			distance = distance + 1
+		}
+	}
+
+	return distance, nil
+
 }
