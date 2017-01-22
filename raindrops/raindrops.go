dfd23dce4de54c3c594b033d627937660c749247
JohnRoach <johnroach1985@gmail.com>
2017-01-21 23:30:18 -0500

    Adding raindrops solution

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,7 +1,31 @@
 package raindrops
 
+import (
+	"bytes"
+	"strconv"
+)
+
 const testVersion = 2
 
-func Convert(int) string
+// Convert converts numbers to a string
+func Convert(convertMe int) string {
+	var buffer bytes.Buffer
 
-// The test program has a benchmark too.  How fast does your Convert convert?
+	if (convertMe % 3) == 0 {
+		buffer.WriteString("Pling")
+	}
+
+	if (convertMe % 5) == 0 {
+		buffer.WriteString("Plang")
+	}
+
+	if (convertMe % 7) == 0 {
+		buffer.WriteString("Plong")
+	}
+
+	if buffer.Len() == 0 {
+		buffer.WriteString(strconv.Itoa(convertMe))
+	}
+
+	return buffer.String()
+}
