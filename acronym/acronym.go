6dffedd1ab098347f602981b56ed3dd4c79b6f56
JohnRoach <johnroach1985@gmail.com>
2017-01-28 00:28:43 -0500

    Adding acronym solution

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,5 +1,23 @@
 package acronym
 
+import (
+	"bytes"
+	"regexp"
+	"strings"
+)
+
 const testVersion = 2
 
-func Abbreviate(string) string
+// Abbreviate create abbreviations for a given string
+func Abbreviate(abbreviateMe string) string {
+	var buffer bytes.Buffer
+
+	re := regexp.MustCompile("\\b[A-Z]{1}|\\b\\w|[A-Z]([a-z])")
+	splitme := re.FindAllString(abbreviateMe, -1)
+
+	for index := range splitme {
+		buffer.WriteString(strings.ToUpper(string([]rune(splitme[index])[0])))
+	}
+
+	return buffer.String()
+}
