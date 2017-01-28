6585ffed13ff54e64637f6e3a26bb94b95dca2a8
JohnRoach <johnroach1985@gmail.com>
2017-01-28 00:58:20 -0500

    Adding solution for pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,36 @@
 package pangram
 
+import "unicode"
+
 const testVersion = 1
 
-func IsPangram(string) bool
+// mappedAlphabet maps the alphabet to false values for default
+func mappedAlphabet() map[rune]bool {
+	makeMap := make(map[rune]bool, 28)
+	for i := 'A'; i <= 'Z'; i++ {
+		//Set all mapped values as false first
+		makeMap[i] = false
+	}
+	return makeMap
+}
+
+// IsPangram checks if a given sentence is a pangram or not.
+func IsPangram(sentence string) bool {
+	mappedAlphabet := mappedAlphabet()
+	for _, character := range []rune(sentence) {
+		if unicode.IsLetter(character) {
+			formatedLetter := unicode.ToUpper(character)
+			if _, goodLetter := mappedAlphabet[formatedLetter]; goodLetter {
+				mappedAlphabet[formatedLetter] = true
+			}
+		}
+	}
+
+	// check if one of the mapped values still doesn't have true
+	for _, value := range mappedAlphabet {
+		if !value {
+			return false
+		}
+	}
+	return true
+}
