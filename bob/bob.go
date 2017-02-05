caa51aba6613d65ed570c0900d99357974903a39
JohnRoach <johnroach1985@gmail.com>
2017-02-04 21:27:41 -0500

    Had overly engineered stuff :(

--- bob/bob.go
+++ bob/bob.go
@@ -1,23 +1,18 @@
 package bob // package name must match the package name in bob_test.go
-import (
-	"regexp"
-	"strings"
-)
+import "strings"
 
 const testVersion = 2
 
 // Hey will return an answer to preceding conversation
 func Hey(converse string) string {
-	cleanConverse := strings.Trim(converse, " \n\r\t\v\u00a0\u2002")
-
-	maybeYell, _ := regexp.MatchString(`\b[A-Z][^a-z].*\b`, cleanConverse)
-
+	cleanConverse := strings.TrimSpace(converse)
 	if cleanConverse == "" {
 		return "Fine. Be that way!"
-	} else if (cleanConverse == strings.ToUpper(cleanConverse)) && maybeYell {
+	}
+	if strings.ToUpper(cleanConverse) == cleanConverse && strings.ToLower(cleanConverse) != cleanConverse {
 		return "Whoa, chill out!"
-	} else if isQuestion, _ := regexp.MatchString(`\?$`, cleanConverse); isQuestion {
-		// the trick above made this to be a little faster 429954 ns/op vs 385752 ns/op
+	}
+	if strings.HasSuffix(cleanConverse, "?") {
 		return "Sure."
 	}
 	return "Whatever."
