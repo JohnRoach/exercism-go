994ed178f382718a484a72f98ffbfbc25b365c5a
John Roach <johnroach1985@gmail.com>
2018-01-01 15:17:34 -0500

    Fixing minor issue

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -4,21 +4,21 @@ import (
 	"strings"
 )
 
-var points = map[string]int{
-	"a": 1, "e": 1, "i": 1, "o": 1, "u": 1, "l": 1, "n": 1, "r": 1, "s": 1, "t": 1,
-	"d": 2, "g": 2,
-	"b": 3, "c": 3, "m": 3, "p": 3,
-	"f": 4, "h": 4, "v": 4, "w": 4, "y": 4,
-	"k": 5,
-	"j": 8, "x": 8,
-	"q": 10, "z": 10,
+var points = map[rune]int{
+	'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
+	'd': 2, 'g': 2,
+	'b': 3, 'c': 3, 'm': 3, 'p': 3,
+	'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+	'k': 5,
+	'j': 8, 'x': 8,
+	'q': 10, 'z': 10,
 }
 
 //Score will return a score
 func Score(word string) int {
 	var totalPoints int
 	for _, character := range strings.ToLower(word) {
-		totalPoints += points[string(character)]
+		totalPoints += points[character]
 	}
 	return totalPoints
 }
